edd993bd158f6961297c2b83c9e8c01c3852d0a4
rebeccajwolf <128657575+rebeccajwolf@users.noreply.github.com>
2023-04-21 05:15:39 -0400

    Updated

---
+++ entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nohup gunicorn keep_alive:app --bind 0.0.0.0:8080 &
+sh ./clean_mem.sh ;
+python3 ms_rewards_farmer.py --everyday --session --fast --headless --telegram "${TOKEN}" "${CHAT_ID}" ;
